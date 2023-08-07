272fdfc686e700e05ebb9f5d13e4454e5c4065c8
Doris Lin <lmh7026@gmail.com>
2023-08-07 10:48:21 -0400

    update register route to display messages

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -23,13 +23,33 @@ router.get(
     }
 );
 
-router.post(
-    '/login', 
-    passport.authenticate('local-login', { session: true }),
-    (req, res, next) => {
-        res.send(req.user)
-    }
-)
+// router.post(
+//     '/login', 
+//     passport.authenticate('local-login', { session: true }),
+//     (req, res, next) => {
+//         res.send(req.user)
+//     }
+// )
+
+router.post('/login', function (req, res, next) {
+    passport.authenticate('local-login', {session: true}, function (err, user, info) {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        // *** Display message without using flash option
+        res.status(401).send(info.message);
+      } else {
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err)
+            }
+            res.status(200).send(user)
+        })
+      }
+    })(req, res, next);
+  });
+  
 
 router.post('/logout', (req: Request, res: Response, next) => {
     res.clearCookie('connect.sid')
@@ -44,12 +64,32 @@ router.post('/logout', (req: Request, res: Response, next) => {
         })
     });
 
-router.post(
-    '/register',
-    passport.authenticate('local-register', { session: true }),
-    (req, res, next) => {
-        res.send(req.user)
-    }
-)
+    
+// router.post(
+//     '/register',
+//     passport.authenticate('local-register', { session: true }),
+//     (req, res, next) => {
+//         res.send(req.user)
+//     }
+// )
+
+router.post('/register', function (req, res, next) {
+    passport.authenticate('local-register', {session: true}, function (err, user, info) {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        // *** Display message without using flash option
+        res.status(401).send(info.message);
+      } else {
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err)
+            }
+            res.status(200).send(user)
+        })
+      }
+    })(req, res, next);
+  });
 
 export default router;
